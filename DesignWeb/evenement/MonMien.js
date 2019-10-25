7afd2d1851ed88214e91b1b47f65e53fd76562d7
MountainDewIt <forget.arthur@carrefour.cegepvicto.ca>
2019-10-25 11:41:32 -0400

    commit du projet 25

---
+++ DesignWeb/evenement/MonMien.js
@@ -0,0 +1,9 @@
+$( "#FriesKetchup" ).hover(function() {
+
+});
+$( "#GangBang" ).hover(function() {
+    //Le code ici sera seulement exécuté lorsque la souris survole target.
+});
+$( "#ChickenFish" ).hover(function() {
+    //Le code ici sera seulement exécuté lorsque la souris survole target.
+});
---
+++ DesignWeb/question6/Forget_Arthur.js
@@ -0,0 +1,31 @@
+var personne = prompt("Combien de personne êtes vous? : ");
+var initial = 10;
+var légumes = prompt("Voulez-vous des légumes sur votre pizza? : ");
+var pepperoni = prompt("Voulez-vous des pepperoni sur votre pizza? : ");
+var sauce = prompt("Voulez-vous de la sauce? : ");
+var TPS = 1.05;
+var TVQ = 1.09975;
+var Pizza;
+
+if( légumes = oui);
+{
+    Pizza = initial + 5);
+}
+else{
+    Pizza = initial;
+}
+if( pepperoni = oui);
+{
+    Pizza = initial + 8;
+}
+else{
+    Pizza = initial;
+}
+if( sauce = oui);
+{
+    Pizza = initial + personne;
+}
+else{
+    Pizza = initial
+}
+document.write("Le prix de votre pizza sera: " + Pizza + "dollars");
